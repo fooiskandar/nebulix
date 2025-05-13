c3e493872a03c8a3d946578a5f391394ce7a1744
fooiskandar <fooiskandar@gmail.com>
2025-05-13 23:37:48 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://chic-ganache-331394.netlify.app",
   integrations: [
     icon(),
     mdx({}),
